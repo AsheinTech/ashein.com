d9184ffbbc4bd92e3a5d843de18387147d315abb
AsheinTech <asheintechnologies@gmail.com>
2025-07-01 16:24:53 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,6 @@
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.148.0/build/three.module.js';
+import { EffectComposer } from 'https://cdn.jsdelivr.net/npm/three@0.148.0/examples/jsm/postprocessing/EffectComposer.js';
+import { RenderPass } from 'https://cdn.jsdelivr.net/npm/three@0.148.0/examples/jsm/postprocessing/RenderPass.js';
+import { UnrealBloomPass } from 'https://cdn.jsdelivr.net/npm/three@0.148.0/examples/jsm/postprocessing/UnrealBloomPass.js';
+
+// your full script code goes here (everything inside window.addEventListener...
