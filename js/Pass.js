168dea8a9fab9a8f1d5749e1f508cd02588bee78
AsheinTech <asheintechnologies@gmail.com>
2025-07-02 12:34:55 +0200

    Create Pass.js

---
+++ js/Pass.js
@@ -0,0 +1,26 @@
+class Pass {
+
+	constructor() {
+		// if set to true, the pass is processed by the composer
+		this.enabled = true;
+
+		// if set to true, the pass indicates to swap read and write buffer after rendering
+		this.needsSwap = true;
+
+		// if set to true, the pass clears its buffer before rendering
+		this.clear = false;
+
+		// if set to true, the result of the pass is rendered to screen
+		this.renderToScreen = false;
+	}
+
+	setSize(width, height) {
+		// resize logic for custom passes
+	}
+
+	render(renderer, writeBuffer, readBuffer /*, deltaTime, maskActive */) {
+		console.warn('THREE.Pass: .render() must be implemented in derived pass.');
+	}
+}
+
+export { Pass };
