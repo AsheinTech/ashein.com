8b9490e628b513cec4d01adbc98f2c99118b779a
AsheinTech <asheintechnologies@gmail.com>
2025-07-02 11:28:10 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import * as THREE from "./js/three.module.js";
 import { EffectComposer } from "./js/EffectComposer.js";
 import { RenderPass } from "./js/RenderPass.js";
-import { UnrealBloomPass } from "../js/UnrealBloomPass.js";
+import { UnrealBloomPass } from "./js/UnrealBloomPass.js";
 
 window.addEventListener("DOMContentLoaded", () => {
   const canvas = document.getElementById('bg');
